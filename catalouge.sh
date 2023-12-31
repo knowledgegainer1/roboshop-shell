c2cd3c8a1fb67270cec74e52f10d11d49f595c36
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-31 13:03:29 +0530

    added

--- catalouge.sh
+++ catalouge.sh
@@ -20,43 +20,45 @@ if [ $id -ne 0 ]; then
     exit 1
 fi
 
-
-
-
-dnf module disable nodejs -y  &>> $logfile
+dnf module disable nodejs -y &>>$logfile
 validate $? "disabled"
-dnf module enable nodejs:18 -y  &>> $logfile
+dnf module enable nodejs:18 -y &>>$logfile
 validate $? "enabled"
 
-dnf install nodejs -y  &>> $logfile
+dnf install nodejs -y &>>$logfile
 validate $? "installing node js latest verison"
 
-useradd roboshop  &>> $logfile
-mkdir /app  &>> $logfile
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip  &>> $logfile
-cd /app   &>> $logfile
-unzip /tmp/catalogue.zip  &>> $logfile
-npm install   &>> $logfile
+id roboshop
+if [ $? -eq 0 ]; then
+    echo "user exists so Skipped"
+else
+    useradd roboshop &>>$logfile
+    validate $? "adding new user"
+fi
+
+mkdir -p /app &>>$logfile
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$logfile
+cd /app &>>$logfile
+unzip -o /tmp/catalogue.zip &>>$logfile
+npm install &>>$logfile
 validate $? "user added, dir created downloaded unzipped and installed dependencies"
- #here cat.service  path shoulb be linux ,git pulled path and what is stoerd in my pc path both should be matched
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>> $logfile
+#here cat.service  path shoulb be linux ,git pulled path and what is stoerd in my pc path both should be matched
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$logfile
 validate $? "copied"
 
-systemctl daemon-reload  &>> $logfile
-systemctl enable catalogue  &>> $logfile
-systemctl start catalogue  &>> $logfile
+systemctl daemon-reload &>>$logfile
+systemctl enable catalogue &>>$logfile
+systemctl start catalogue &>>$logfile
 validate $? "restared"
 #think that you are in linux folder after gil pull ,then tou r chanign to ect folder
-cp /home/centos/roboshop-shell/mongo.repo etc/yum.repos.d/mongo.repo  &>> $logfile
+cp /home/centos/roboshop-shell/mongo.repo etc/yum.repos.d/mongo.repo &>>$logfile
 
-
-yum list installed mongodb-org-shell  &>>$logfile
+yum list installed mongodb-org-shell &>>$logfile
 if [ $? -eq 0 ]; then
-    echo -e  "Already installed so .. $Y SKIPPED INSTALLATION $N"
+    echo -e "Already installed so .. $Y SKIPPED INSTALLATION $N"
 else
     dnf install mongodb-org-shell -y &>>$logfile
     validate $? "Installed "
 fi
 
-
-mongo --host mongo.ssrg.online </app/schema/catalogue.js  &>> $logfile
+mongo --host mongo.ssrg.online </app/schema/catalogue.js &>>$logfile
