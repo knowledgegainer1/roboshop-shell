fa9c5114449533a0094c51c975e3e1455a49cbdb
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-31 17:27:12 +0530

    added

---
+++ redis.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+date=$(date +%F-%R-%S)
+logfile="/tmp/$0-$date.log"
+id=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+validate() {
+    if [ $1 -eq 1 ]; then
+        echo -e " $R FAILED ...$N   $2 check logs at  /tmp/$0-$date.log"
+        exit 1
+    else
+        echo -e "$G Successfully $2 $N"
+    fi
+}
+if [ $id -ne 0 ]; then
+    echo -e "Dont have permission to do this !! $R Please run this by SUDO access $N"
+    exit 1
+fi
+
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+validate $? "installed version 8"
+
+dnf module enable redis:remi-6.2 -y
+validate  $? "enabled 6.2"
+
+dnf install redis -y
+validate  $? " installed redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g'    /etc/redis.conf
+validate  $? " changes URL "
+sed -i 's//127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+validate  $? " changes URL redis"
+systemctl enable redis
+validate  $? "enabled redis"
+systemctl start redis
+validate  $? " started redis"
+systemctl restart redis
+validate  $? " restarted redis"
